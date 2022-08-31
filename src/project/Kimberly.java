5d59c4aebc12cad1092c21535a526da992e966e9
Luzvin Urban <lucybdk@gmail.com>
2022-08-31 13:43:04 -0400

    Second class created

---
+++ src/project/Kimberly.java
@@ -0,0 +1,8 @@
+package project;
+
+public class Kimberly {
+    public static void main(String[] args) {
+        System.out.println("Believe in yourself");
+        System.out.println("Giving up is not an potion");
+    }
+}
--- src/project/Lucy.java
+++ src/project/Lucy.java
@@ -1,6 +1,6 @@
 package project;
 
-public class Saim {
+public class Lucy {
     public static void main(String[] args) {
         System.out.println("Keep Going");
     }
